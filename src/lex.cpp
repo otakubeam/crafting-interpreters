782726a2142f3c1afe4363ce3f852c5ac42d915d
Andrew Orazov <orazov.ae@phystech.edu>
2022-07-15 09:06:00 +0300

    Aux functions

--- src/lex.cpp
+++ src/lex.cpp
@@ -1,28 +1,86 @@
 #include <iostream>
+#include <string>
 
-// What's the difference between token and token type?
+//////////////////////////////////////////////////////////////////////
 
 enum TokenType {
-    NUMBER,
-    STRING,
+  NUMBER,
+  STRING,
 
-    PLUS,
-    MINUS,
+  PLUS,
+  MINUS,
 
-    PRINT,
-    FUN, // FUN foo
-    VAR, // VAR name
-    // Wait, what?? name and foo should instead be its own things
-    // perhaps TokenType ID
-    IDENTIFIER,
+  PRINT,
+  FUN,  // FUN foo
+  VAR,  // VAR name
+  // Wait, what?? name and foo should instead be its own things
+  // perhaps TokenType ID
+  IDENTIFIER,
+
+  // These only change the state of the lexer
+  TOKEN_EOL,
+  TOKEN_EOF,
 };
 
+//////////////////////////////////////////////////////////////////////
 
-// I wanna take a char stream
-// and convert it into tokens
-//
-// TokenType NextToken(ScanInfo& info) {
-//      // info contains the string we are trying to lex
-//      // and our current position (anything else?)
-// }
-//
+struct Location {
+  int file = 0;
+
+  // What token can span more that a line?
+  // None, right?
+
+  int lineno = 0;
+  int columnno = 0;
+};
+
+struct ScanInfo {
+  char* stream_buf;
+  struct Location loc;
+};
+
+void BufferMore(ScanInfo& info) {
+  // ...
+}
+
+char CurrentSymbol(struct ScanInfo& info) {
+  return info.stream_buf[info.loc.columnno];
+}
+
+bool IsWhitespace(char ch) {
+  return ch == '\n' || ch == ' ' || ch == '\t';
+}
+
+void MoveRight(ScanInfo& info) {
+  if (CurrentSymbol(info) == EOF) {
+  }
+
+  if (CurrentSymbol(info) == '\n') {
+  } else {
+  }
+}
+
+void SkipWhitespace(ScanInfo& info) {
+  while (IsWhitespace(CurrentSymbol(info))) {
+    ;
+  }
+}
+
+//////////////////////////////////////////////////////////////////////
+
+struct Token {
+  enum TokenType type;
+  struct Location loc;
+  union {
+    int value;
+    std::string name;
+  };
+};
+
+//////////////////////////////////////////////////////////////////////
+
+struct Token NextToken(ScanInfo& info) {
+  while (SkipWhitespace(info)) {
+    ;
+  }
+}
