a9a6d066d8dc75a90691615f2cf524978cad22d7
Andrew Orazov <orazov.ae@phystech.edu>
2022-07-22 11:33:50 +0300

    Use fmt lib

--- src/parse/test/test.cpp
+++ src/parse/test/test.cpp
@@ -3,27 +3,37 @@
 
 #include <parse/parse.hpp>
 
+#include <fmt/core.h>
 #include <iostream>
 
-int main() {
-  lex::Location loc;
+bool TestEvaluator() {
+
+  lex::Location loc_dummy;
   auto ty = lex::TokenType::NUMBER;
 
-  auto tk = lex::Token{ty, loc, 1}, tk2 = lex::Token{ty, loc, 2};
-  auto a1 = LiteralExpression{tk}, a2 = LiteralExpression{tk2};
+  auto minus_tk = lex::Token{lex::TokenType::MINUS, loc_dummy};
+  auto plus_tk  = lex::Token{lex::TokenType::PLUS, loc_dummy};
 
-  auto minus_tk = lex::Token{lex::TokenType::MINUS, loc};
-  auto plus_tk = lex::Token{lex::TokenType::PLUS, loc};
+  auto tk = lex::Token{ty, loc_dummy, 1},//
+      tk2 = lex::Token{ty, loc_dummy, 2};
 
+  auto a1 = LiteralExpression{tk}, //
+       a2 = LiteralExpression{tk2};
   auto un1 = UnaryExpression{minus_tk, &a1};
-  auto bin1 = BinaryExpression{&a1, minus_tk, &a2};
-  auto bin2 = BinaryExpression{&un1, plus_tk, &bin1};
+  auto bin1 = BinaryExpression{&a1, minus_tk, &a2}, //
+       bin2 = BinaryExpression{&un1, plus_tk, &bin1};
 
   Evaluator e;  
 
-  auto result2 = GetPrim<int>(e.Eval(&bin1));
-  std::cout << result2 << std::endl;
+  auto result1 = GetPrim<int>(e.Eval(&bin1));
+  fmt::print("Result is {}, expected is {}\n", result1, -1);
 
-  auto result = GetPrim<int>(e.Eval(&bin2));
-  std::cout << result << std::endl;
+  auto result2 = GetPrim<int>(e.Eval(&bin2));
+  fmt::print("Result is {}, expected is {}\n", result2, -2);
+
+  return true;
+}
+
+int main() {
+   TestEvaluator();
 }
